e7bc3984e22e4fc4a68bcfab830b06b980871cfc
Shenwy <swydesky@hotmail.com>
2019-12-04 15:21:32 +0800

    first commit

---
+++ src/main/java/com/tf/market/MarketController.java
@@ -0,0 +1,13 @@
+package com.tf.market;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author swy
+ * @date 2019/12/04
+ */
+@RestController
+public class MarketController {
+
+
+}
